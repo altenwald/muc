299535e74fc61737d9f7f464645c5b7cdaedafa8
Manuel Rubio <manuel@jongla.com>
2014-01-17 17:07:38 +0200

    disco items

--- apps/muc/src/muc.erl
+++ apps/muc/src/muc.erl
@@ -3,7 +3,21 @@
 
 -export([process_iq/1]).
 
+-include_lib("exmpp/include/exmpp.hrl").
 -include_lib("ecomponent/include/ecomponent.hrl").
+
+process_iq(#params{from=From, iq=IQ, ns=?NS_DISCO_ITEMS, type="get"}) ->
+    FromBin = exmpp_jid:to_binary(exmpp_jid:make(From)),
+    lager:debug("get room list", []),
+    Rooms = muc_db:list_rooms(FromBin),
+    Items = lists:map(fun([Name, JID]) ->
+        exmpp_xml:element(undefined, <<"item">>, [
+        	{<<"jid">>, JID}, {<<"name">>, Name}
+        ], [])
+    end, Rooms),
+    Result = exmpp_iq:result(IQ, Items),
+    ecomponent:send(Result, ?NS_DISCO_ITEMS, muc, false),
+    ok;
 
 process_iq(#params{}=Params) ->
     lager:info("Params arrived: ~p~n", [Params]),
