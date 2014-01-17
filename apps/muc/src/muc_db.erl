80a1e731c9d7ebc25d0620ea303333df945a9d89
Manuel Rubio <manuel@altenwald.com>
2014-01-17 17:07:38 +0200

    disco items

---
+++ apps/muc/src/muc_db.erl
@@ -0,0 +1,19 @@
+-module(muc_db).
+
+-export([list_rooms/1]).
+
+-define(POOL, "ejabberd").
+
+-define(LIST_ROOMS, 
+    "SELECT name, jid "
+    "FROM rooms "
+    "WHERE id IN ( "
+        "SELECT rooms_id "
+        "FROM room_users "
+        "WHERE member_jid = ? ) ").
+
+list_rooms(JID) ->
+    {ok, C} = epgsql_pool:get_connection(?POOL),
+    {ok, _Columns, Rows} = pgsql:equery(C, ?LIST_ROOMS, [JID]),
+    epgsql_pool:return_connection(?POOL, C),
+    Rows.
