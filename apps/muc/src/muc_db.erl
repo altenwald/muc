2fa7bbc29f652f2459f7d610dcde014dac59624d
Manuel Rubio <manuel@altenwald.com>
2014-01-20 15:33:26 +0000

    minor fixes

--- apps/muc/src/muc_db.erl
+++ apps/muc/src/muc_db.erl
@@ -1,4 +1,5 @@
 -module(muc_db).
+-compile([warnings_as_errors]).
 
 -export([list_rooms/1]).
 
@@ -10,10 +11,10 @@
     "WHERE id IN ( "
         "SELECT rooms_id "
         "FROM room_users "
-        "WHERE member_jid = ? ) ").
+        "WHERE member_jid = $1 ) ").
 
 list_rooms(JID) ->
-    {ok, C} = epgsql_pool:get_connection(?POOL),
+    {ok, C} = pgsql_pool:get_connection(?POOL),
     {ok, _Columns, Rows} = pgsql:equery(C, ?LIST_ROOMS, [JID]),
-    epgsql_pool:return_connection(?POOL, C),
+    pgsql_pool:return_connection(?POOL, C),
     Rows.
